b43b932fb8742ecc188e20a5562fe4aae719bc5c
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-02-03 22:11:23 +0700

    adding material ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
-  return <div className="text-red-700">Hello world</div>
+  return (
+  <div>
+    <div className="text-red-700">Hello world</div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </div>
+  )
 }
 
 export default App;
