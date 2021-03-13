3e862a378f8b7e7f5034bef23105767e78f44c8d
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-03-13 23:55:22 +0700

    implement my image cropper

--- src/components/Shared/MyModal.tsx
+++ src/components/Shared/MyModal.tsx
@@ -3,23 +3,19 @@ import ClearIcon from "@material-ui/icons/Clear";
 import * as React from "react";
 
 interface Props {
-  isModalOpen: boolean;
-  setIsModalOpen: (bool: boolean) => void;
+  open: boolean;
+  setOpen: (bool: boolean) => void;
   children: any;
 }
 
-const MyModal: React.FC<Props> = ({
-  isModalOpen,
-  setIsModalOpen,
-  children,
-}) => {
+const MyModal: React.FC<Props> = ({ open, setOpen, children }) => {
   return (
-    <Modal open={isModalOpen}>
+    <Modal open={open}>
       <div className="absolute w-max max-w-lg h-72 inset-0 m-auto bg-white rounded-2xl outline-none">
         <div className="flex flex-col justity-center items-center py-4 px-6">
           <div className="flex justify-between">
             <button
-              onClick={() => setIsModalOpen(false)}
+              onClick={() => setOpen(false)}
               className="focus:outline-none mr-4"
             >
               <span>
--- src/components/Shared/MyPhotoUpload.tsx
+++ src/components/Shared/MyPhotoUpload.tsx
@@ -1,17 +1,39 @@
 import { Button } from "@material-ui/core";
 import AddPhotoIcon from "@material-ui/icons/AddPhotoAlternate";
 import * as React from "react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FileReaderResultType } from "types";
+import MyImageCropper from "./MyImageCropper";
 import MyModal from "./MyModal";
 
 interface Props {}
 
 const MyPhotoUpload: React.FC<Props> = () => {
-  const [photoInput, setPhotoInput] = useState("");
+  const [photoInput, setPhotoInput] = useState<Blob | null>(null);
+  const [previewSource, setPreviewSource] = useState<FileReaderResultType>(
+    null
+  );
   const photoInputEl = useRef<HTMLInputElement>(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPhotoEditModalOpen, setIsPhotoEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (photoInput) {
+      checkImageValid(photoInput);
+    }
+  }, [photoInput]);
+
+  const checkImageValid = (photoInput: Blob) => {
+    if (width < 800 || height < 1200) {
+      console.log("width", width);
+      console.log("height", height);
+      setIsModalOpen(true);
+    } else {
+      previewFile(photoInput);
+    }
+  };
 
   const handlePhotoInputChange = (e: any) => {
     const file = e.target.files[0];
@@ -20,9 +42,15 @@ const MyPhotoUpload: React.FC<Props> = () => {
     img.onload = () => {
       setWidth(img.naturalWidth);
       setHeight(img.naturalHeight);
-      if (width < 800 || height < 1200) {
-        setIsModalOpen(true);
-      }
+      setPhotoInput(file);
+    };
+  };
+
+  const previewFile = (file: Blob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSource(reader.result);
     };
   };
 
@@ -30,19 +58,46 @@ const MyPhotoUpload: React.FC<Props> = () => {
     <div>
       <label htmlFor="photo-input">
         <div className="h-60 w-40 border border-dashed flex justify-center items-center cursor-pointer">
-          <AddPhotoIcon />
+          {previewSource ? (
+            <>
+              <img
+                className="w-full h-full"
+                src={previewSource as string}
+                alt="cover"
+              />
+              <button onClick={() => setIsPhotoEditModalOpen(true)}>
+                Edit
+              </button>
+              <div>
+                <MyModal
+                  open={isPhotoEditModalOpen}
+                  setOpen={setIsPhotoEditModalOpen}
+                >
+                  {{
+                    header: "Chỉnh sửa ảnh",
+                    content: (
+                      <>
+                        <MyImageCropper src={previewSource as string} />
+                      </>
+                    ),
+                  }}
+                </MyModal>
+              </div>
+            </>
+          ) : (
+            <AddPhotoIcon />
+          )}
         </div>
         <input
           type="file"
           ref={photoInputEl}
           accept=".jpg,.jpeg,.png.gif"
-          value={photoInput}
           onChange={handlePhotoInputChange}
           id="photo-input"
           className="hidden"
         />
       </label>
-      <MyModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+      <MyModal open={isModalOpen} setOpen={setIsModalOpen}>
         {{
           header: "Tải lên một bức ảnh có chất lượng tốt hơn",
           content: (
