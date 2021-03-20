867d46f933a16a4ebf04c5f688d81669d362313d
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-03-20 23:22:37 +0700

    implement check valid date func

--- src/pages/client/experience/ExperienceActivationPage/ExperienceActivationPage.tsx
+++ src/pages/client/experience/ExperienceActivationPage/ExperienceActivationPage.tsx
@@ -6,8 +6,57 @@ interface Props {}
 
 const ExperienceActivationPage: React.FC<Props> = () => {
   const [value, setValue] = useState<DateObject | DateObject[]>();
+  const [value2, setValue2] = useState<DateObject | DateObject[]>();
 
-  return <DatePicker multiple value={value} onChange={setValue} />;
+  const isDateValid = (
+    pickedDates: DateObject | DateObject[],
+    date: DateObject
+  ) => {
+    if (pickedDates instanceof DateObject) {
+      let isValid = pickedDates.dayOfYear === date.dayOfYear;
+      if (!isValid)
+        return {
+          disabled: true,
+          style: { color: "#ccc" },
+          onClick: () => alert("weekends are disabled"),
+        };
+    } else {
+      const daysOfYear = pickedDates.map((item) => item.dayOfYear);
+      let isValid = daysOfYear?.includes(date.dayOfYear);
+      if (!isValid)
+        return {
+          disabled: true,
+          style: { color: "#ccc" },
+          onClick: () => alert("weekends are disabled"),
+        };
+    }
+  };
+
+  return (
+    <>
+      <DatePicker
+        multiple
+        animation
+        type="icon"
+        format="DD/MM/YYYY"
+        value={value}
+        onChange={setValue}
+      />
+      <DatePicker
+        mapDays={({ date }) => {
+          if (value) {
+            return isDateValid(value, date);
+          }
+        }}
+        multiple
+        animation
+        type="icon"
+        format="DD/MM/YYYY"
+        value={value2}
+        onChange={setValue2}
+      />
+    </>
+  );
 };
 
 export default ExperienceActivationPage;
