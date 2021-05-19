766c962a4230dbcf9e504b139ec2beb18bf4f546
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-05-19 23:27:37 +0700

    update admin dashboard

--- src/pages/admin/DashBoard.tsx
+++ src/pages/admin/DashBoard.tsx
@@ -3,102 +3,120 @@ import LocalActivityOutlinedIcon from "@material-ui/icons/LocalActivityOutlined"
 import PermIdentityOutlinedIcon from "@material-ui/icons/PermIdentityOutlined";
 import PieChartOutlinedIcon from "@material-ui/icons/PieChartOutlined";
 import WorkOutlineIcon from "@material-ui/icons/WorkOutline";
+import MyLoadingIndicator from "components/Shared/MyLoadingIndicator";
+import { useActivities } from "hooks/queries/activities";
+import { useExperiences } from "hooks/queries/experiences";
+import { useUsers } from "hooks/queries/users";
 import * as React from "react";
 
 interface Props {}
 
 const DashBoard: React.FC<Props> = () => {
+  const { data: experiences } = useExperiences();
+  const { data: activities } = useActivities();
+  const { data: users } = useUsers();
+
   return (
-    <div className="grid md:grid-cols-4 lg:grid-cols-12 gap-2">
-      <div className="md:col-span-2 lg:col-span-3">
-        <div className="bg-white p-4 flex items-center justify-between rounded-sm">
-          <div>
-            <div>
-              <Typography
-                className="text-2xl font-bold mb-1"
-                style={{ color: "#e62265" }}
-              >
-                278
-              </Typography>
+    <>
+      {experiences && activities && users ? (
+        <>
+          <div className="grid md:grid-cols-4 lg:grid-cols-12 gap-2">
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="bg-white p-4 flex items-center justify-between rounded-sm">
+                <div>
+                  <div>
+                    <Typography
+                      className="text-2xl font-bold mb-1"
+                      style={{ color: "#e62265" }}
+                    >
+                      {experiences.length}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography>New Experiences</Typography>
+                  </div>
+                </div>
+                <div>
+                  <WorkOutlineIcon
+                    style={{ width: 50, height: 50, color: "#e62265" }}
+                  />
+                </div>
+              </div>
             </div>
-            <div>
-              <Typography>New Experiences</Typography>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="bg-white p-4 flex items-center justify-between rounded-sm">
+                <div>
+                  <div>
+                    <Typography
+                      className="text-2xl font-bold mb-1"
+                      style={{ color: "#229e8f" }}
+                    >
+                      {users.length}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography>New Users</Typography>
+                  </div>
+                </div>
+                <div>
+                  <PermIdentityOutlinedIcon
+                    style={{ width: 50, height: 50, color: "#229e8f" }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="bg-white p-4 flex items-center justify-between rounded-sm">
+                <div>
+                  <div>
+                    <Typography
+                      className="text-2xl font-bold mb-1"
+                      style={{ color: "#ff5720" }}
+                    >
+                      {activities.length}
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography>New Activities</Typography>
+                  </div>
+                </div>
+                <div>
+                  <LocalActivityOutlinedIcon
+                    style={{ width: 50, height: 50, color: "#ff5720" }}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="md:col-span-2 lg:col-span-3">
+              <div className="bg-white p-4 flex items-center justify-between rounded-sm">
+                <div>
+                  <div>
+                    <Typography
+                      className="text-2xl font-bold mb-1"
+                      style={{ color: "#14b8d2" }}
+                    >
+                      64,78%
+                    </Typography>
+                  </div>
+                  <div>
+                    <Typography>Good Feedback</Typography>
+                  </div>
+                </div>
+                <div>
+                  <PieChartOutlinedIcon
+                    style={{ width: 50, height: 50, color: "#14b8d2" }}
+                  />
+                </div>
+              </div>
             </div>
           </div>
-          <div>
-            <WorkOutlineIcon
-              style={{ width: 50, height: 50, color: "#e62265" }}
-            />
-          </div>
+        </>
+      ) : (
+        <div className="mx-auto">
+          <MyLoadingIndicator width={300} height={300} />
         </div>
-      </div>
-      <div className="md:col-span-2 lg:col-span-3">
-        <div className="bg-white p-4 flex items-center justify-between rounded-sm">
-          <div>
-            <div>
-              <Typography
-                className="text-2xl font-bold mb-1"
-                style={{ color: "#229e8f" }}
-              >
-                141
-              </Typography>
-            </div>
-            <div>
-              <Typography>New Users</Typography>
-            </div>
-          </div>
-          <div>
-            <PermIdentityOutlinedIcon
-              style={{ width: 50, height: 50, color: "#229e8f" }}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="md:col-span-2 lg:col-span-3">
-        <div className="bg-white p-4 flex items-center justify-between rounded-sm">
-          <div>
-            <div>
-              <Typography
-                className="text-2xl font-bold mb-1"
-                style={{ color: "#ff5720" }}
-              >
-                243
-              </Typography>
-            </div>
-            <div>
-              <Typography>New Activities</Typography>
-            </div>
-          </div>
-          <div>
-            <LocalActivityOutlinedIcon
-              style={{ width: 50, height: 50, color: "#ff5720" }}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="md:col-span-2 lg:col-span-3">
-        <div className="bg-white p-4 flex items-center justify-between rounded-sm">
-          <div>
-            <div>
-              <Typography
-                className="text-2xl font-bold mb-1"
-                style={{ color: "#14b8d2" }}
-              >
-                64,78%
-              </Typography>
-            </div>
-            <div>
-              <Typography>Good Feedback</Typography>
-            </div>
-          </div>
-          <div>
-            <PieChartOutlinedIcon
-              style={{ width: 50, height: 50, color: "#14b8d2" }}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
