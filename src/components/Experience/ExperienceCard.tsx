7489b56368dab003f58d5ae83b8c659cc36cde65
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-05-27 23:25:27 +0700

    update experience card

--- src/components/Experience/ExperienceCard.tsx
+++ src/components/Experience/ExperienceCard.tsx
@@ -1,4 +1,6 @@
+import StarIcon from "@material-ui/icons/Star";
 import { useExperience } from "hooks/queries/experiences";
+import { useCountReview } from "hooks/queries/reviews";
 import * as React from "react";
 import { Link } from "react-router-dom";
 
@@ -8,9 +10,10 @@ interface Props {
 
 const ExperienceCard: React.FC<Props> = ({ experienceId }) => {
   const { data: experience } = useExperience(experienceId);
+  const { data: reviews } = useCountReview(experienceId);
   return (
     <>
-      {experience ? (
+      {experience && reviews ? (
         <Link to={`/experience/${experienceId}`}>
           <img
             style={{ width: 200, height: 300 }}
@@ -18,11 +21,26 @@ const ExperienceCard: React.FC<Props> = ({ experienceId }) => {
             src={experience.photoGallery![0]?.url}
             alt="description"
           />
+          <div style={{ maxWidth: 200, height: 40 }}>
+            <div className="mt-2">
+              <span>
+                <StarIcon className="text-sm text-secondary" />
+              </span>
+              <span className="text-sm text-gray-500 mx-1">
+                {reviews.averageStars}
+              </span>
+              <span className="text-sm text-gray-500">
+                ({reviews.totalItems})
+              </span>
+              <span>·</span>
+              <span className="text-sm text-gray-500 mx-1">
+                {experience.address?.city}
+              </span>
+            </div>
+          </div>
           <div style={{ maxWidth: 200 }}>
-            <div className="mt-2 text-center">
-              <p className="text-sm text-gray-700 font-bold">
-                {experience.title}
-              </p>
+            <div className="mt-2">
+              <span className="text-sm font-bold">{experience.title}</span>
             </div>
           </div>
         </Link>
