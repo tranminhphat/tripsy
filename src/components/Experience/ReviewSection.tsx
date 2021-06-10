dec8ce550d83de73a12e7332aac20fc50fd3e987
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-06-10 22:54:44 +0700

    update

--- src/components/Experience/ReviewSection.tsx
+++ src/components/Experience/ReviewSection.tsx
@@ -6,110 +6,115 @@ import MyLoadingIndicator from "components/Shared/MyLoadingIndicator";
 import MyTruncateText from "components/Shared/MyTruncateText";
 import * as React from "react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
-  experienceId: string;
+	experienceId: string;
 }
 
 const PAGE_SIZE = 4;
 
 const ReviewSection: React.FC<Props> = ({ experienceId }) => {
-  const [reviews, setReviews] = useState<any[]>([]);
-  const [count, setCount] = useState<{
-    totalItems: number;
-    averageStars: number;
-  }>();
-  const [pageNumber, setPageNumber] = useState(2);
-  const [isFetching, setIsFetching] = useState<boolean | null>(false);
+	const [reviews, setReviews] = useState<any[]>([]);
+	const [count, setCount] =
+		useState<{
+			totalItems: number;
+			averageStars: number;
+		}>();
+	const [pageNumber, setPageNumber] = useState(2);
+	const [isFetching, setIsFetching] = useState<boolean | null>(false);
 
-  useEffect(() => {
-    fetchInitialData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+	useEffect(() => {
+		fetchInitialData();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  const fetchInitialData = async () => {
-    const { data } = await countReviews({ objectId: experienceId });
-    if (data) {
-      setCount(data);
-    }
-    const {
-      data: { items },
-    } = await getReviews({ objectId: experienceId }, "", 1, PAGE_SIZE);
-    if (items) {
-      setReviews([...reviews, ...items]);
-    }
-  };
+	const fetchInitialData = async () => {
+		const { data } = await countReviews({ objectId: experienceId });
+		if (data) {
+			setCount(data);
+		}
+		const {
+			data: { items },
+		} = await getReviews({ objectId: experienceId }, "", 1, PAGE_SIZE);
+		if (items) {
+			console.log(items);
+			setReviews([...reviews, ...items]);
+		}
+	};
 
-  const loadMoreData = async () => {
-    setIsFetching(true);
-    const {
-      data: { items },
-    } = await getReviews({ objectId: experienceId }, "", pageNumber, PAGE_SIZE);
-    if (items.length !== 0) {
-      setReviews([...reviews, ...items]);
-      setPageNumber(pageNumber + 1);
-      setIsFetching(false);
-    } else {
-      setIsFetching(null);
-    }
-  };
+	const loadMoreData = async () => {
+		setIsFetching(true);
+		const {
+			data: { items },
+		} = await getReviews({ objectId: experienceId }, "", pageNumber, PAGE_SIZE);
+		if (items.length !== 0) {
+			setReviews([...reviews, ...items]);
+			setPageNumber(pageNumber + 1);
+			setIsFetching(false);
+		} else {
+			setIsFetching(null);
+		}
+	};
 
-  return (
-    <div>
-      {reviews && count ? (
-        <>
-          <div className="flex items-center">
-            <span className="mr-2 text-xl font-bold">
-              <StarIcon style={{ width: 30, height: 30 }} />
-            </span>
-            <span className="text-xl font-bold">
-              {count.averageStars} ({count.totalItems} đánh giá)
-            </span>
-          </div>
-          <div className="mt-4 grid lg:grid-cols-2">
-            {reviews.map((item) => (
-              <div className="lg:col-span-1" key={item._id}>
-                <div className="mt-4">
-                  <div className="flex items-center">
-                    <div className="mr-2">
-                      <Avatar
-                        src={
-                          item.user.avatarUrl
-                            ? item.user.avatarUrl
-                            : SkeletonUserAvatar
-                        }
-                        style={{ width: "48px", height: "48px" }}
-                        alt="User"
-                      />
-                    </div>
-                    <div className="font-bold">
-                      {item.user.lastName} {item.user.firstName}
-                    </div>
-                  </div>
-                  <div className="mt-2">
-                    <MyTruncateText text={item.content} />
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div className="text-center">
-            {isFetching === null ? (
-              <p className="my-2 text-gray-500">Đã hiển thị tất cả đánh giá</p>
-            ) : !isFetching ? (
-              <button className="my-2" onClick={() => loadMoreData()}>
-                <p className="underline">Xem thêm </p>
-              </button>
-            ) : (
-              <p className="my-2">Đang tải...</p>
-            )}
-          </div>
-        </>
-      ) : (
-        <MyLoadingIndicator />
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{reviews && count ? (
+				<>
+					<div className="flex items-center">
+						<span className="mr-2 text-xl font-bold">
+							<StarIcon style={{ width: 30, height: 30 }} />
+						</span>
+						<span className="text-xl font-bold">
+							{count.averageStars} ({count.totalItems} đánh giá)
+						</span>
+					</div>
+					<div className="mt-4 grid lg:grid-cols-2">
+						{reviews.map((item) => (
+							<div className="lg:col-span-1" key={item._id}>
+								<div className="mt-4">
+									<div className="flex items-center">
+										<div className="mr-2">
+											<Link to={`/user/profile/${item.user._id}`}>
+												<Avatar
+													src={
+														item.user.avatarUrl
+															? item.user.avatarUrl
+															: SkeletonUserAvatar
+													}
+													style={{ width: "48px", height: "48px" }}
+													alt="User"
+												/>
+											</Link>
+										</div>
+										<div className="font-bold">
+											{item.user.lastName} {item.user.firstName}
+										</div>
+									</div>
+									<div className="mt-2">
+										<MyTruncateText text={item.content} />
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+					<div className="text-center">
+						{isFetching === null ? (
+							<p className="my-2 text-gray-500">Đã hiển thị tất cả đánh giá</p>
+						) : !isFetching ? (
+							<button className="my-2" onClick={() => loadMoreData()}>
+								<p className="underline">Xem thêm </p>
+							</button>
+						) : (
+							<p className="my-2">Đang tải...</p>
+						)}
+					</div>
+				</>
+			) : (
+				<MyLoadingIndicator />
+			)}
+		</div>
+	);
 };
 
 export default ReviewSection;
--- src/pages/client/profile/UserInformation/UserInformation.tsx
+++ src/pages/client/profile/UserInformation/UserInformation.tsx
@@ -14,221 +14,223 @@ import { useContext, useState } from "react";
 import OverflowWrapper from "react-overflow-wrapper";
 import { Link } from "react-router-dom";
 interface Props {
-  userData: IUser;
-  isCurrentUser: boolean;
+	userData: IUser;
+	isCurrentUser: boolean;
 }
 
 const UserInformation: React.FC<Props> = ({ userData, isCurrentUser }) => {
-  const { data: profile } = useProfile(userData.profileId);
-  const { data: experiences } = useExperiences({ hostId: userData._id });
-  const updateProfile = useUpdateProfile();
-  const [isCreatingIntroduction, setIsCreatingIntroduction] = useState(false);
-  const { alert } = useContext(AlertContext);
+	const { data: profile } = useProfile(userData.profileId);
+	const { data: experiences } = useExperiences({ hostId: userData._id });
+	const updateProfile = useUpdateProfile();
+	const [isCreatingIntroduction, setIsCreatingIntroduction] = useState(false);
+	const { alert } = useContext(AlertContext);
 
-  const handleIntroductionUpdate = async (values) => {
-    updateProfile.mutate(
-      { profileId: profile?._id, values },
-      {
-        onSuccess: () => {
-          alert("success", "Cập nhật thành công");
-        },
-        onError: () => {
-          alert("success", "Cập nhật thất bại");
-        },
-      }
-    );
-  };
+	const handleIntroductionUpdate = async (values) => {
+		updateProfile.mutate(
+			{ profileId: profile?._id, values },
+			{
+				onSuccess: () => {
+					alert("success", "Cập nhật thành công");
+				},
+				onError: () => {
+					alert("success", "Cập nhật thất bại");
+				},
+			}
+		);
+	};
 
-  return (
-    <>
-      <div>
-        <div>
-          <Typography className="text-4xl">
-            Xin chào, tôi là {userData.firstName}
-          </Typography>
-        </div>
-        {!isCreatingIntroduction ? (
-          <div className="mt-2">
-            {profile?.introduction ? (
-              <div className="whitespace-pre-line">
-                <MyTruncateText text={profile?.introduction} />
-              </div>
-            ) : (
-              <div>Bạn chưa có lời giới thiệu</div>
-            )}
-            {isCurrentUser ? (
-              <div className="mt-2">
-                <p
-                  className="underline hover:no-underline cursor-pointer"
-                  onClick={() => setIsCreatingIntroduction(true)}
-                >
-                  Cập nhật lời giới thiệu
-                </p>
-              </div>
-            ) : null}
-          </div>
-        ) : (
-          <div className="mt-2">
-            <Formik
-              initialValues={{ introduction: profile?.introduction }}
-              onSubmit={(values) => {
-                handleIntroductionUpdate(values);
-              }}
-            >
-              {() => (
-                <Form>
-                  <div>
-                    <Field
-                      as="textarea"
-                      name="introduction"
-                      className="w-full h-36 p-4 border border-gray-300"
-                    />
-                  </div>
-                  <div className="flex justify-end">
-                    <Button
-                      className="bg-primary text-white"
-                      variant="contained"
-                      type="submit"
-                    >
-                      Cập nhật
-                    </Button>
-                    <Button
-                      className="ml-2 border border-success text-success"
-                      variant="outlined"
-                      onClick={() => setIsCreatingIntroduction(false)}
-                    >
-                      Xong
-                    </Button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
-        )}
-      </div>
+	return (
+		<>
+			<div>
+				<div>
+					<Typography className="text-4xl">
+						Xin chào, tôi là {userData.firstName}
+					</Typography>
+				</div>
+				{!isCreatingIntroduction ? (
+					<div className="mt-2">
+						{profile?.introduction ? (
+							<div className="whitespace-pre-line">
+								<MyTruncateText text={profile?.introduction} />
+							</div>
+						) : (
+							<>
+								{isCurrentUser ? <div>Bạn chưa có lời giới thiệu</div> : null}
+							</>
+						)}
+						{isCurrentUser ? (
+							<div className="mt-2">
+								<p
+									className="underline hover:no-underline cursor-pointer"
+									onClick={() => setIsCreatingIntroduction(true)}
+								>
+									Cập nhật lời giới thiệu
+								</p>
+							</div>
+						) : null}
+					</div>
+				) : (
+					<div className="mt-2">
+						<Formik
+							initialValues={{ introduction: profile?.introduction }}
+							onSubmit={(values) => {
+								handleIntroductionUpdate(values);
+							}}
+						>
+							{() => (
+								<Form>
+									<div>
+										<Field
+											as="textarea"
+											name="introduction"
+											className="w-full h-36 p-4 border border-gray-300"
+										/>
+									</div>
+									<div className="flex justify-end">
+										<Button
+											className="bg-primary text-white"
+											variant="contained"
+											type="submit"
+										>
+											Cập nhật
+										</Button>
+										<Button
+											className="ml-2 border border-success text-success"
+											variant="outlined"
+											onClick={() => setIsCreatingIntroduction(false)}
+										>
+											Xong
+										</Button>
+									</div>
+								</Form>
+							)}
+						</Formik>
+					</div>
+				)}
+			</div>
 
-      <div className="mt-4">
-        <hr />
-      </div>
-      <div className="mt-4">
-        {profile && profile.checkpoints.some((item) => item.points === 100) ? (
-          <>
-            <div>
-              <Typography className="text-xl font-bold">Danh hiệu</Typography>
-              {profile.checkpoints.map((item) => {
-                const theme = themes.find((theme) => theme.id === item.themeId);
-                const themeName = theme?.name;
-                if (item.points === 100) {
-                  return (
-                    <div className="mt-4 flex items-center">
-                      <img src={MedalIcon} alt="medal" width={32} height={32} />
-                      <Typography className="ml-2 text-lg">
-                        {themeName}
-                      </Typography>
-                    </div>
-                  );
-                } else {
-                  return null;
-                }
-              })}
-            </div>
-            <div className="mt-4">
-              <hr />
-            </div>
-          </>
-        ) : null}
-      </div>
-      <div className="mt-4">
-        {experiences && experiences.length !== 0 ? (
-          <>
-            <div>
-              <Typography className="text-xl font-bold">
-                Trải nghiệm của {userData.firstName}
-              </Typography>
-              <OverflowWrapper
-                iconColor="#000"
-                iconSize={48}
-                iconStyle={{
-                  left: { marginTop: "100px" },
-                  right: { marginTop: "100px" },
-                }}
-              >
-                <div className="flex mt-4">
-                  {experiences.map((item) => (
-                    <Link key={item._id!} to={`/experience/${item._id}`}>
-                      <div style={{ width: 150 }} className="mx-2">
-                        <img
-                          style={{ width: 150, height: 200 }}
-                          className="rounded-md"
-                          src={item.photoGallery![0].url}
-                          alt="experience"
-                        />
-                      </div>
-                      <div>
-                        <div className="whitespace-normal mt-2 text-center">
-                          <p>{item.title}</p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
-              </OverflowWrapper>
-            </div>
-            <div className="mt-4">
-              <hr />
-            </div>
-          </>
-        ) : null}
+			<div className="mt-4">
+				<hr />
+			</div>
+			<div className="mt-4">
+				{profile && profile.checkpoints.some((item) => item.points === 100) ? (
+					<>
+						<div>
+							<Typography className="text-xl font-bold">Danh hiệu</Typography>
+							{profile.checkpoints.map((item) => {
+								const theme = themes.find((theme) => theme.id === item.themeId);
+								const themeName = theme?.name;
+								if (item.points === 100) {
+									return (
+										<div className="mt-4 flex items-center">
+											<img src={MedalIcon} alt="medal" width={32} height={32} />
+											<Typography className="ml-2 text-lg">
+												{themeName}
+											</Typography>
+										</div>
+									);
+								} else {
+									return null;
+								}
+							})}
+						</div>
+						<div className="mt-4">
+							<hr />
+						</div>
+					</>
+				) : null}
+			</div>
+			<div className="mt-4">
+				{experiences && experiences.length !== 0 ? (
+					<>
+						<div>
+							<Typography className="text-xl font-bold">
+								Trải nghiệm của {userData.firstName}
+							</Typography>
+							<OverflowWrapper
+								iconColor="#000"
+								iconSize={48}
+								iconStyle={{
+									left: { marginTop: "100px" },
+									right: { marginTop: "100px" },
+								}}
+							>
+								<div className="flex mt-4">
+									{experiences.map((item) => (
+										<Link key={item._id!} to={`/experience/${item._id}`}>
+											<div style={{ width: 150 }} className="mx-2">
+												<img
+													style={{ width: 150, height: 200 }}
+													className="rounded-md"
+													src={item.photoGallery![0].url}
+													alt="experience"
+												/>
+											</div>
+											<div>
+												<div className="whitespace-normal mt-2 text-center">
+													<p>{item.title}</p>
+												</div>
+											</div>
+										</Link>
+									))}
+								</div>
+							</OverflowWrapper>
+						</div>
+						<div className="mt-4">
+							<hr />
+						</div>
+					</>
+				) : null}
 
-        <div className="mt-4">
-          {profile && profile.savedExperiencesList?.length !== 0 ? (
-            <>
-              <div>
-                <Typography className="text-xl font-bold">
-                  Trải nghiệm yêu thích của {userData.firstName}
-                </Typography>
-                <OverflowWrapper
-                  iconColor="#000"
-                  iconSize={48}
-                  iconStyle={{
-                    left: { marginTop: "100px" },
-                    right: { marginTop: "100px" },
-                  }}
-                >
-                  <div className="flex mt-4">
-                    {profile.savedExperiencesList?.map((item) => (
-                      <Link key={item._id!} to={`/experience/${item._id}`}>
-                        <div style={{ width: 150 }} className="mx-2">
-                          <div>
-                            <img
-                              style={{ width: 150, height: 200 }}
-                              className="rounded-md"
-                              src={item.photoGallery![0].url}
-                              alt="experience"
-                            />
-                          </div>
-                          <div className="mt-2 whitespace-normal text-center">
-                            {item.title}
-                          </div>
-                        </div>
-                      </Link>
-                    ))}
-                  </div>
-                </OverflowWrapper>
-              </div>
+				<div className="mt-4">
+					{profile && profile.savedExperiencesList?.length !== 0 ? (
+						<>
+							<div>
+								<Typography className="text-xl font-bold">
+									Trải nghiệm yêu thích của {userData.firstName}
+								</Typography>
+								<OverflowWrapper
+									iconColor="#000"
+									iconSize={48}
+									iconStyle={{
+										left: { marginTop: "100px" },
+										right: { marginTop: "100px" },
+									}}
+								>
+									<div className="flex mt-4">
+										{profile.savedExperiencesList?.map((item) => (
+											<Link key={item._id!} to={`/experience/${item._id}`}>
+												<div style={{ width: 150 }} className="mx-2">
+													<div>
+														<img
+															style={{ width: 150, height: 200 }}
+															className="rounded-md"
+															src={item.photoGallery![0].url}
+															alt="experience"
+														/>
+													</div>
+													<div className="mt-2 whitespace-normal text-center">
+														{item.title}
+													</div>
+												</div>
+											</Link>
+										))}
+									</div>
+								</OverflowWrapper>
+							</div>
 
-              <div className="mt-4">
-                <hr />
-              </div>
-            </>
-          ) : null}
-        </div>
-      </div>
-      <div className="mt-4">
-        <ReviewSection userId={userData._id!} />
-      </div>
-    </>
-  );
+							<div className="mt-4">
+								<hr />
+							</div>
+						</>
+					) : null}
+				</div>
+			</div>
+			<div className="mt-4">
+				<ReviewSection userId={userData._id!} />
+			</div>
+		</>
+	);
 };
 
 export default UserInformation;
