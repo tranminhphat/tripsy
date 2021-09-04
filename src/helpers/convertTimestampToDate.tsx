ce738c58c0bfde11b2795c735384b1c2ca9e1cb0
Phat Tran <62209890+miph2004@users.noreply.github.com>
2021-09-05 00:44:39 +0700

    update editor config

--- src/helpers/convertTimestampToDate.tsx
+++
@@ -1,7 +0,0 @@
-const convertTimestampToDate = (timestamp: string) => {
-	const d = new Date(timestamp);
-	const date = d.getHours() + ":" + d.getMinutes() + ", " + d.toDateString();
-	return date;
-};
-
-export default convertTimestampToDate;
